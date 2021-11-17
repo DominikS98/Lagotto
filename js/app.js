00e64705b9260aa6cde0edd56bfa2dd204491e25
Dominik Szojda <szojda.98@gmail.com>
2021-11-17 19:23:14 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,18 +8,25 @@ const psyToggleMenu = document.querySelector('.psy__toggle__menu');
 const psySubmenu = document.querySelector('.psysubmenu');
 const psyBoxSubmenu = document.querySelector('.Psy__box__submenu');
 
+const cookie = document.querySelector(".Cbutton");
+const cookiebox = document.querySelector('.cookieBox');
 
 
-toggle.addEventListener('click', () => {
+cookie.addEventListener('click', function () {
+    cookiebox.classList.add('cookie--off');
+});
+
+
+toggle.addEventListener('click', function () {
     menuBox.classList.toggle('menuOn');
-})
-toggleMenu.addEventListener('click', () => {
+});
+toggleMenu.addEventListener('click', function () {
     submenu.classList.toggle('submenuOn');
     boxSubmenu.classList.toggle('Bigbox__submenu');
 
-})
-psyToggleMenu.addEventListener('click', () => {
+});
+psyToggleMenu.addEventListener('click', function () {
     psySubmenu.classList.toggle('submenuOn');
     psyBoxSubmenu.classList.toggle('Bigbox__submenuS');
 
-})
+});
